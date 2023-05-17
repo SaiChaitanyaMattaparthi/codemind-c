d7fdbc7394cd9cd0817811e9b601338d385ef674
SaiChaitanyaMattaparthi <119723737+SaiChaitanyaMattaparthi@users.noreply.github.com>
2023-05-17 22:46:16 +0530

    #include<stdio.h> int main() {     int n,d,k=0;     scanf("%d",&n);     while(n!=0)     {         d=n%10;         n=n/10; if(k<d) {     k=d; }     }     printf("%d",k);     return 0; }

---
+++ Largest_Digit.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n,d,k=0;
+    scanf("%d",&n);
+    while(n!=0)
+    {
+        d=n%10;
+        n=n/10;
+if(k<d)
+{
+    k=d;
+}
+    }
+    printf("%d",k);
+    return 0;
+}
