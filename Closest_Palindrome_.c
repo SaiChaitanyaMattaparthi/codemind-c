c9421e0b21c369f092917fe494eabc65bc2aa7b8
SaiChaitanyaMattaparthi <119723737+SaiChaitanyaMattaparthi@users.noreply.github.com>
2023-05-20 23:43:07 +0530

    #include<stdio.h> int fun(int n) {     int t,d,r=0;     t=n;     while(t)     {         d=t%10;         r=r*10+d;         t=t/10;
        }
        if(r==n)
        {
            return 1;
            
        }
        else
        {
            return 0;
            
        }
        
    }
    int main()
    {
        int a,b,i,j,n,t;
        scanf("%d",&n);
        for(i=n+1;i<n+10000;i++)
        {
            if(fun(i)==1){a=i;break;
                
            }
            
        }
        for(i=n-1;i>0;i--)
        {
            if(fun(i)==1)
            {
                b=i;break;
                
            }
            
        }
        if((n-b)==(a-n))
        {
            printf("%d %d",b,a);
            
        }
        else if((n-b)<(a-n))
    {
        printf("%d",b);
        
    }
    else{printf("%d",a);
        
    }
        
    }

--- Closest_Palindrome_.c
+++ Closest_Palindrome_.c
@@ -8,15 +8,19 @@ int fun(int n)
         d=t%10;
         r=r*10+d;
         t=t/10;
+        
     }
     if(r==n)
     {
         return 1;
+        
     }
     else
     {
         return 0;
+        
     }
+    
 }
 int main()
 {
@@ -24,30 +28,32 @@ int main()
     scanf("%d",&n);
     for(i=n+1;i<n+10000;i++)
     {
+        if(fun(i)==1){a=i;break;
+            
+        }
+        
+    }
+    for(i=n-1;i>0;i--)
+    {
         if(fun(i)==1)
         {
-            a=i;
-            break;
+            b=i;break;
+            
         }
+        
     }
-        for(i=n-1;i>0;i--)
-        {
-            if(fun(i)==1)
-            {
-                b=i;
-                break;
-            }
-        }
-        if((n-b)==(a-n))
-        {
-            printf("%d %d",b,a);
-        }
-        else if((n-b)<(a-n))
+    if((n-b)==(a-n))
+    {
+        printf("%d %d",b,a);
+        
+    }
+    else if((n-b)<(a-n))
 {
     printf("%d",b);
+    
 }
-else
-{
-    printf("%d",a);
+else{printf("%d",a);
+    
 }
+    
 }
