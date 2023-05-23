d564f007dffe6d03b8e57192f0a9b298faeae6ea
SaiChaitanyaMattaparthi <119723737+SaiChaitanyaMattaparthi@users.noreply.github.com>
2023-05-24 05:29:36 +0530

    #include <stdio.h> int main ( ) {     int n ;     scanf ( "%d" , &n ) ;     int a [ n ] ;     float s = 0 , t = 0 ;     for ( int i = 0 ; i < n ; i++ )     {         scanf ( "%d" , &a [ i ] ) ;     }     for ( int  i = 0 ; i < n ; i++ )     {         int c = 0 ;         for ( int j = 0 ; j < n ; j++ )         {             if ( a [ i ] == a [ j ] )             {                 c++ ;             }         }         if ( c == a [i ] )         {             s = s + a [ i ] ;             t++ ;             a[i]=0;         }     }     if ( s > 0 )     {         printf ( "%0.2f" , s / t ) ;     }     else     {         printf ( "-1"); } }

--- Average_of_super_elements.c
+++ Average_of_super_elements.c
@@ -1,44 +1,37 @@
-#include<stdio.h>
-int main()
+#include <stdio.h>
+int main ( )
 {
-    int n, arr[100];
-    int skip[100];
-    int sum = 0; 
-    int items = 0;
-    double res;
-    scanf("%d",&n);
-    for(int i =0 ;i < n; ++i)
+    int n ;
+    scanf ( "%d" , &n ) ;
+    int a [ n ] ;
+    float s = 0 , t = 0 ;
+    for ( int i = 0 ; i < n ; i++ )
     {
-        scanf("%d",&arr[i]);
-        skip[i] = 0;
-        }
-        for(int i=0; i<n; i++)
+        scanf ( "%d" , &a [ i ] ) ;
+    }
+    for ( int  i = 0 ; i < n ; i++ )
+    {
+        int c = 0 ;
+        for ( int j = 0 ; j < n ; j++ )
         {
-            int count = 0;
-            if (skip[i])
-            continue;
-            for(int j=0; j<n; j++)
+            if ( a [ i ] == a [ j ] )
             {
-                if(arr[i]==arr[j])
-                {
-                    ++count;
-                    skip[j] = 1;
-                    }
-                    }
-                    if(arr[i] == count)
-                    {
-                        sum += arr[i];
-                        ++items;
-                        }
-                        }
-                        if (items == 0)
-                        {
-                            printf("-1"); 
-                        }
-                        else 
-                        {
-                            res = ((double)sum)/items;
-printf("%.2f",res);
+                c++ ;
+            }
+        }
+        if ( c == a [i ] ) 
+        {
+            s = s + a [ i ] ;
+            t++ ;
+            a[i]=0;
+        }
+    }
+    if ( s > 0 )
+    {
+        printf ( "%0.2f" , s / t ) ;
+    }
+    else
+    {
+        printf ( "-1");
 }
-return 0;
 }
